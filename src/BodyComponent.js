bb9c2e981510934e69ff3157aad1069fa7b1f591
Dhanabalan <sed9089@sis.nyp.org>
2022-04-25 16:46:23 -0400

    contentful integration

--- src/BodyComponent.js
+++ src/BodyComponent.js
@@ -42,10 +42,9 @@ function BodyComponent() {
       console.log(item.fields.name);
       let rawRichTextField = item.fields.description;
       console.log(documentToHtmlString(rawRichTextField));
-      let imageUrl="https://placeholder.pics/svg/300";
-      if (item.fields.image && item.fields.image.length >0)
-      {
-          imageUrl = item.fields.image[0].fields.file.url;
+      let imageUrl = "https://placeholder.pics/svg/300";
+      if (item.fields.image && item.fields.image.length > 0) {
+        imageUrl = item.fields.image[0].fields.file.url;
       }
 
       contentsArray.push(<Row style={{ marginTop: "20px", marginBottom: "20px" }}>
@@ -64,8 +63,8 @@ function BodyComponent() {
             <div className="col-md-12">
               {/* <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(rawRichTextField) }}> */}
               <div>
-              {documentToReactComponents(item.fields.description, renderOptions)}
-              {/* {documentToReactComponents(item.fields.description, renderOptions)} */}
+                {documentToReactComponents(item.fields.description, renderOptions)}
+                {/* {documentToReactComponents(item.fields.description, renderOptions)} */}
               </div>
             </div>
           </div>
@@ -81,12 +80,35 @@ function BodyComponent() {
   const renderOptions = {
     renderNode: {
       [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+          // target the contentType of the EMBEDDED_ENTRY to display as you need
+      if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+        return (
+          <pre>
+            <code>{node.data.target.fields.code}</code>
+          </pre>
+        );
+      }
+
+      if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+        return (
+          <iframe
+            src={node.data.target.fields.embedUrl}
+            height="100%"
+            width="100%"
+            frameBorder="0"
+            scrolling="no"
+            title={node.data.target.fields.title}
+            allowFullScreen={true}
+          />
+        );
+      }
       },
 
       [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
         // render the EMBEDDED_ASSET as you need
+
         return (
-          <img
+          <Image fluid
             src={`https://${node.data.target.fields.file.url}`}
             height={node.data.target.fields.file.details.image.height}
             width={node.data.target.fields.file.details.image.width}
@@ -101,42 +123,6 @@ function BodyComponent() {
     <div>
       <Container >
         {getContents()}
-        {/* {this.fetchPlayers()} */}
-        {/* <Row style={{ marginTop: "20px", marginBottom: "20px" }}>
-            <Col md={4} >
-              <Image src="https://source.unsplash.com/500x200/?cars" fluid />
-            </Col>
-            <Col md={8}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-            </Col>
-          </Row>
-          <Row style={{ marginTop: "20px", marginBottom: "20px" }}>
-
-            <Col md={8}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-            </Col>
-            <Col md={4} >
-              <Image src="https://source.unsplash.com/500x200/?dogs" fluid />
-            </Col>
-          </Row>
-
-          <Row style={{ marginTop: "20px", marginBottom: "20px" }}>
-            <Col md={4} >
-              <Image src="https://source.unsplash.com/500x200/?ship" fluid />
-            </Col>
-            <Col md={8}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-            </Col>
-          </Row>
-
-          <Row style={{ marginTop: "20px", marginBottom: "20px" }}>
-            <Col md={8}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-            </Col>
-            <Col md={4} >
-              <Image src="https://source.unsplash.com/500x200/?plane" fluid />
-            </Col>
-          </Row> */}
       </Container>
     </div >
   );
