cc848927e0f79643c76583b3e18b607470e1162c
Dhanabalan <sed9089@sis.nyp.org>
2022-04-26 15:50:30 -0400

    carousel

--- src/HomeComponent.js
+++ src/HomeComponent.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
+import Carousel from 'react-bootstrap/Carousel'
 
 import * as contentful from 'contentful';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
@@ -123,7 +124,50 @@ function HomeComponent() {
 
   return (
     <div>
-      <Container >
+      <Container  >
+
+        <Row className="mt-5">
+          <Col>
+          <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="https://source.unsplash.com/random/780x400?sky" fluid rounded
+      alt="First slide"
+
+
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src="https://source.unsplash.com/random/780x400?falls" fluid rounded
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://source.unsplash.com/random/780x400?water" fluid rounded
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+          </Col>
+          </Row>
+
         {getContents()}
       </Container>
     </div >
