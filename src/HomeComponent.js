4235e58c9bdbfe7acb9698a61389e4430c8d89d2
Dhanabalan <sed9089@sis.nyp.org>
2022-04-27 22:30:07 -0400

    again

--- src/HomeComponent.js
+++ src/HomeComponent.js
@@ -6,8 +6,8 @@ import React, { useState, useEffect } from 'react';
 // import Carousel from 'react-bootstrap/Carousel'
 
 import * as contentful from 'contentful';
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+// import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 function HomeComponent() {
@@ -58,7 +58,7 @@ function HomeComponent() {
   }
 
   const getContents = () => {
-    const contentsArray = []
+    // const contentsArray = []
 
 
     // console.log(splitItems(items));
@@ -160,49 +160,49 @@ function HomeComponent() {
     </div>;
   }
 
-  const renderOptions = {
-    renderNode: {
-      [INLINES.EMBEDDED_ENTRY]: (node, children) => {
-        // target the contentType of the EMBEDDED_ENTRY to display as you need
-        if (node.data.target.sys.contentType.sys.id === "codeBlock") {
-          return (
-            <pre>
-              <code>{node.data.target.fields.code}</code>
-            </pre>
-          );
-        }
+  // const renderOptions = {
+  //   renderNode: {
+  //     [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+  //       // target the contentType of the EMBEDDED_ENTRY to display as you need
+  //       if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+  //         return (
+  //           <pre>
+  //             <code>{node.data.target.fields.code}</code>
+  //           </pre>
+  //         );
+  //       }
 
-        if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
-          return (
-            <iframe
-              src={node.data.target.fields.embedUrl}
-              height="100%"
-              width="100%"
-              frameBorder="0"
-              scrolling="no"
-              title={node.data.target.fields.title}
-              allowFullScreen={true}
-            />
-          );
-        }
-      },
+  //       if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+  //         return (
+  //           <iframe
+  //             src={node.data.target.fields.embedUrl}
+  //             height="100%"
+  //             width="100%"
+  //             frameBorder="0"
+  //             scrolling="no"
+  //             title={node.data.target.fields.title}
+  //             allowFullScreen={true}
+  //           />
+  //         );
+  //       }
+  //     },
 
-      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-        // render the EMBEDDED_ASSET as you need
+  //     [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+  //       // render the EMBEDDED_ASSET as you need
 
-        return (
-          <div className="image-wrapper float-start pe-4 ">
-            <img className="img-fluid rounded"
-              src={`https://${node.data.target.fields.file.url}`}
-              height={node.data.target.fields.file.details.image.height}
-              width={node.data.target.fields.file.details.image.width}
-              alt={node.data.target.fields.description} />
-          </div>
+  //       return (
+  //         <div className="image-wrapper float-start pe-4 ">
+  //           <img className="img-fluid rounded"
+  //             src={`https://${node.data.target.fields.file.url}`}
+  //             height={node.data.target.fields.file.details.image.height}
+  //             width={node.data.target.fields.file.details.image.width}
+  //             alt={node.data.target.fields.description} />
+  //         </div>
 
-        );
-      }
-    }
-  }
+  //       );
+  //     }
+  //   }
+  // }
 
   return (
     <div className="container">
