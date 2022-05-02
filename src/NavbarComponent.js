3f65e068a76ebc12a3c36b4c18835d920cf485ba
Dhanabalan <sed9089@sis.nyp.org>
2022-05-02 12:13:12 -0400

    style

--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -11,46 +11,13 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons'
 function NavbarComponent() {
   return (
     <div>
-      <Container fluid style={{ backgroundImage: "url('https:////images.ctfassets.net/9gf6mhyw2bkx/5I6IUvH0tVDtpw8ox5c6hm/9a30ffbc4d53f50723ace613cfa51979/aboutus_500.jpg')", height: "300px" }}>
-        {/* <Navbar  style={{backgroundColor:"#ce2127"}} expand="lg">
-          <Container>
-            <Navbar.Brand href="/">SG Tailor</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <LinkContainer to="/">
-                  <Nav.Link >Home</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/about">
-                  <Nav.Link>About</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/services">
-                  <Nav.Link>Services</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/contactus">
-                  <Nav.Link>Contact Us</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/allsamples">
-                  <Nav.Link>All Samples</Nav.Link>
-                </LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar> */}
-        <div style={{ paddingTop: "20px" }}></div>
-        <Navbar style={{ backgroundColor: "#ce2127" }} expand="lg">
+      <Container fluid style={{ backgroundImage: "url('https:////images.ctfassets.net/9gf6mhyw2bkx/5I6IUvH0tVDtpw8ox5c6hm/9a30ffbc4d53f50723ace613cfa51979/aboutus_500.jpg')" }}>
+       
+        <div style={{ paddingTop: "200px" }}></div>
+        <Navbar style={{ backgroundColor: "#ce2127",opacity:".8" }} expand="lg">
 
           <Container>
-
-            <Navbar.Brand href="/"> 
-            
-            <img
-          alt=""
-          src="/logosg.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
+            <Navbar.Brand href="/">          
             SGStitches</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
@@ -72,12 +39,12 @@ function NavbarComponent() {
                 </LinkContainer>
               </Nav>
               <Nav>
-                <p style={{ color: 'white', fontSize: "30px" }}>  <FontAwesomeIcon icon={faPhone} style={{ color: "white" }} />  +1 (610) 563 6590  </p>
+                <div style={{ color: 'white', fontSize: "30px" }}>  <FontAwesomeIcon icon={faPhone} style={{ color: "white" }} />  +1 (610) 563 6590  </div>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-      </Container>
+        </Container>
     </div>
   );
 }
